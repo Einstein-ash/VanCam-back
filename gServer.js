f06c2d9574b5d251cbe128bb6d0235f6c5965036
Ashwani <ashwanisingh46572@gmail.com>
2024-09-07 15:52:30 +0530

    handled port

--- gServer.js
+++ gServer.js
@@ -36,10 +36,21 @@ app.use(cors({
 app.use(bodyParser.json({ limit: '50mb' }));  // Increase the limit as needed
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
+// app.use(session({
+//   secret: process.env.SESSION_SECRET,
+//   resave: false,
+//   saveUninitialized: true
+// }));
+
 app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: true,
+  cookie: {
+    secure: true, // Ensure the cookie is only sent over HTTPS
+    httpOnly: true, // Prevent client-side JavaScript from accessing the cookie
+    maxAge: 24 * 60 * 60 * 1000 // Set a reasonable expiration time (e.g., 1 day)
+  }
 }));
 
 app.use(passport.initialize());
@@ -448,4 +459,6 @@ app.get('/logout', (req, res) => {
 });
 
 // Start server
-app.listen(5000, () => console.log('Server running on http://localhost:5000'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT , () => console.log(`Server running on
+  ${PORT} `));
